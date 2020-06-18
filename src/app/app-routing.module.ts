64435e35ab2f0e4d7075d6acfeb1a331b4ef4502
mario-galindo <mgalindo9410@gmail.com>
2020-06-18 17:24:10 -0600

    add landing component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: 'landing', component: LandingComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
